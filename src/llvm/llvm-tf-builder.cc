0c2d2da9e965e04d535d7b334478ea7ec703de17
linzj <zuojian.lzj@alibaba-inc.com>
2019-01-24 16:06:23 +0800

    Fix tc patchpoint bug.

--- src/llvm/llvm-tf-builder.cc
+++ src/llvm/llvm-tf-builder.cc
@@ -420,7 +420,6 @@ void TCCallResolver::BuildCall(const CallDescriptor& call_desc) {
       EmitCallInstr(output().repo().patchpointVoidIntrinsic(),
                     patchpoint_operands.data(), patchpoint_operands.size());
   LLVMSetInstructionCallConv(patchpoint_ret, LLVMV8CallConv);
-  LLVMSetTailCall(patchpoint_ret, true);
 }
 
 bool TCCallResolver::isTailCall() { return true; }
